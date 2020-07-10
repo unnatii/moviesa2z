bf857fd1a076f36c43a4c4aa4f54bf9c25a13eb9
Unnatii Tibrewal <unnatitibrewal@gmail.com>
2020-07-10 21:54:05 +0530

    Updates

---
+++ precache-manifest.95bf21673e3090835126ebf03dc68352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8c51dc91f740c511f724351b55c42f",
+    "url": "/moviesa2z/index.html"
+  },
+  {
+    "revision": "eb050b241333a7418a74",
+    "url": "/moviesa2z/static/css/main.dd5a3217.chunk.css"
+  },
+  {
+    "revision": "22b9cb958c8371760d6a",
+    "url": "/moviesa2z/static/js/2.f7528d79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moviesa2z/static/js/2.f7528d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb050b241333a7418a74",
+    "url": "/moviesa2z/static/js/main.7cfe081f.chunk.js"
+  },
+  {
+    "revision": "d3673bee593d4040d72a",
+    "url": "/moviesa2z/static/js/runtime-main.5a06f877.js"
+  }
+]);
